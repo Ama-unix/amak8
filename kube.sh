87d01e58bcb02058b893fded13926eaf5bd67ad7
Ama-unix <137280331+Ama-unix@users.noreply.github.com>
2023-07-08 16:35:01 +0200

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -36,4 +36,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
+
+sudo usermod -aG sudo $USER
 sudo usermod -aG docker ${USER}
